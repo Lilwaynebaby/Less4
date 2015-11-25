82c615e86dae4d27eb361c39343f169b9dde7f60
Kosterin <kosterin.vova@mail.ru>
2015-11-25 04:02:00 +0200

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator{
+    public int result;
+
+    public void add(int first,int second){
+        result = first+second;
+    }
+    public void sub(int first,int second){
+        result = first-second;
+    }
+    public void div(int first,int second){
+        result = first/second;
+    }
+    public void mult(int first,int second){
+        result = first*second;
+    }
+    public int getResult(){
+        return result;
+    }
+    public void clearResult(){
+        this.result = 0;
+    }
+
+}
